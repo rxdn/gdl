7fbfb4c59426748bb39db1e5948074b1e5bf05e8
Dot-Rar <dotrar@protonmail.com>
2020-04-14 19:08:09 +0100

    rename intents

--- gateway/intents.go
+++ gateway/intents.go
@@ -3,19 +3,19 @@ package gateway
 type Intent int
 
 const (
-	Guilds Intent = 1 << iota
-	GuildMembers
-	GuildBans
-	GuildEmojis
-	GuildIntegrations
-	GuildWebhooks
-	GuildInvites
-	GuildVoiceStates
-	GuildPresences
-	GuildMessages
-	GuildMessageReactions
-	GuildMessageTyping
-	DirectMessages
-	DirectMessageReactions
-	DirectMessageTyping
+	IntentGuilds Intent = 1 << iota
+	IntentGuildMembers
+	IntentGuildBans
+	IntentGuildEmojis
+	IntentGuildIntegrations
+	IntentGuildWebhooks
+	IntentGuildInvites
+	IntentGuildVoiceStates
+	IntentGuildPresences
+	IntentGuildMessages
+	IntentGuildMessageReactions
+	IntentGuildMessageTyping
+	IntentDirectMessages
+	IntentDirectMessageReactions
+	IntentDirectMessageTyping
 )
