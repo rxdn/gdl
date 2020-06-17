428d121e036e3933ed11d59cef779a3d37fa9468
rxdn <dotrar@protonmail.com>
2020-06-17 21:02:43 +0100

    make it compile

--- objects/auditlog/auditlog.go
+++ objects/auditlog/auditlog.go
@@ -2,11 +2,13 @@ package auditlog
 
 import (
 	"github.com/rxdn/gdl/objects/guild"
+	"github.com/rxdn/gdl/objects/integration"
 	"github.com/rxdn/gdl/objects/user"
 )
 
 type AuditLog struct {
-	Webhooks []guild.Webhook `json:"webhooks"`
-	Users    []user.User     `json:"users"`
-	Entries
+	Webhooks     []guild.Webhook           `json:"webhooks"`
+	Users        []user.User               `json:"users"`
+	Entries      []AuditLogEntry           `json:"audit_log_entries"`
+	Integrations []integration.Integration `json:"integrations"`
 }
