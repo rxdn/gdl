e348f6dce060c54b5caff3b869bf4e144ef18122
rxdn <29165304+rxdn@users.noreply.github.com>
2020-10-27 22:13:10 +0000

    Fix untracked files

---
+++ rest/ratelimit/routes.go
@@ -0,0 +1,165 @@
+package ratelimit
+
+type RouteType uint8
+
+const (
+	RouteTypeGuild RouteType = iota
+	RouteTypeChannel
+	RouteTypeWebhook
+	RouteTypeOther
+)
+
+type RouteId uint16
+
+type Route struct {
+	Type      RouteType
+	Id        RouteId
+	Snowflake uint64
+}
+
+func NewRoute(routeType RouteType, id RouteId, snowflake uint64) Route {
+	return Route{
+		Type:      routeType,
+		Id:        id,
+		Snowflake: snowflake,
+	}
+}
+
+func NewGuildRoute(id RouteId, snowflake uint64) Route {
+	return NewRoute(RouteTypeGuild, id ,snowflake)
+}
+
+func NewChannelRoute(id RouteId, snowflake uint64) Route {
+	return NewRoute(RouteTypeChannel, id ,snowflake)
+}
+
+func NewWebhookRoute(id RouteId, snowflake uint64) Route {
+	return NewRoute(RouteTypeWebhook, id ,snowflake)
+}
+
+func NewOtherRoute(id RouteId, snowflake uint64) Route {
+	return NewRoute(RouteTypeOther, id ,snowflake)
+}
+
+const (
+	// /guilds/:id/audit-logs
+	RouteGetGuildAuditLog RouteId = iota
+
+	// /channels/:id/...
+	RouteGetChannel
+	RouteModifyChannel
+	RouteDeleteChannel
+	RouteGetChannelMessages
+	RouteGetChannelMessage
+	RouteCreateMessage
+	RouteCrosspostMessage
+	RouteCreateReaction
+	RouteDeleteOwnReaction
+	RouteDeleteUserReaction
+	RouteGetReactions
+	RouteDeleteAllReactions
+	RouteDeleteAllReactionsForEmoji
+	RouteEditMessage
+	RouteDeleteMessage
+	RouteBulkDeleteMessages
+	RouteEditChannelPermissions
+	RouteGetChannelInvites
+	RouteCreateChannelInvite
+	RouteDeleteChannelPermission
+	RouteFollowNewsChannel
+	RouteTriggerTypingIndicator
+	RouteGetPinnedMessages
+	RouteAddPinnedChannelMessage
+	RouteDeletePinnedChannelMessage
+	RouteGroupDMAddRecipient
+	RouteGroupDMRemoveRecipient
+
+	// /guilds/:id/emojis
+	RouteListGuildEmojis
+	RouteGetGuildEmoji
+	RouteCreateGuildEmoji
+	RouteModifyGuildEmoji
+	RouteDeleteGuildEmoji
+
+	// /guilds/:id/...
+	RouteCreateGuild
+	RouteGetGuild
+	RouteGetGuildPreview
+	RouteModifyGuild
+	RouteDeleteGuild
+	RouteGetGuildChannels
+	RouteCreateGuildChannel
+	RouteModifyGuildChannelPositions
+	RouteGetGuildMember
+	RouteListGuildMembers
+	RouteAddGuildMember
+	RouteModifyGuildMember
+	RouteModifyCurrentUserNick
+	RouteAddGuildMemberRole
+	RouteRemoveGuildMemberRole
+	RouteRemoveGuildMember
+	RouteGetGuildBans
+	RouteGetGuildBan
+	RouteCreateGuildBan
+	RouteRemoveGuildBan
+	RouteGetGuildRoles
+	RouteCreateGuildRole
+	RouteModifyGuildRolePositions
+	RouteModifyGuildRole
+	RouteDeleteGuildRole
+	RouteGetGuildPruneCount
+	RouteBeginGuildPrune
+	RouteGetGuildVoiceRegions
+	RouteGetGuildInvites
+	RouteGetGuildIntegrations
+	RouteCreateGuildIntegration
+	RouteModifyGuildIntegration
+	RouteDeleteGuildIntegration
+	RouteSyncGuildIntegration
+	RouteGetGuildWidgetSettings
+	RouteModifyGuildWidget
+	RouteGetGuildWidget
+	RouteGetGuildVanityURL
+	RouteGuildWidgetImage
+
+	// /invites/:id
+	// Invites seemingly don't have ratelimits, but we need these enums internally
+	RouteGetInvite
+	RouteDeleteInvite
+
+	// /guilds/templates/:code
+	// Also seemingly no ratelimits
+	RouteGetTemplate
+	RouteCreateGuildTemplate
+
+	// /users/:id/...
+	// Again, seemingly no ratelimits but we need these internally
+	RouteGetCurrentUser
+	RouteGetUser
+	RouteModifyCurrentUser
+	RouteGetCurrentUserGuilds
+	RouteLeaveGuild
+	RouteGetUserDMs
+	RouteCreateDM
+	RouteCreateGroupDM
+	RouteGetUserConnections
+
+	// /voice/regions
+	RouteListVoiceRegions
+
+	// /channels/:id/webhooks
+	RouteCreateWebhook
+	RouteGetChannelWebhooks
+
+	// /guilds/:id/webhooks
+	RouteGetGuildWebhooks
+
+	// /webhooks/:id/...
+	RouteGetWebhook
+	RouteGetWebhookWithToken
+	RouteModifyWebhook
+	RouteModifyWebhookWithToken
+	RouteDeleteWebhook
+	RouteDeleteWebhookWithToken
+	RouteExecuteWebhook
+)
