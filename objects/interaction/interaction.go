a94a7796f585f54bb9f200aba48c3e335be3bb99
rxdn <29165304+rxdn@users.noreply.github.com>
2020-12-14 15:21:23 +0000

    Decode snowflakes correctly

--- objects/interaction/interaction.go
+++ objects/interaction/interaction.go
@@ -6,8 +6,8 @@ type Interaction struct {
 	Id        uint64                             `json:"id,string"`
 	Type      InteractionType                    `json:"type"`
 	Data      *ApplicationCommandInteractionData `json:"data"`
-	GuildId   uint64                             `json:"guild_id"`
-	ChannelId uint64                             `json:"channel_id"`
+	GuildId   uint64                             `json:"guild_id,string"`
+	ChannelId uint64                             `json:"channel_id,string"`
 	Member    member.Member                      `json:"member"`
 	Token     string                             `json:"token"`
 }
