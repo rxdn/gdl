d84ed0db0f5c8c31c072ae17e49e51a38dd1c479
rxdn <29165304+rxdn@users.noreply.github.com>
2021-03-01 22:15:08 +0000

    Remove omitempty

--- objects/channel/message/messagereference.go
+++ objects/channel/message/messagereference.go
@@ -4,5 +4,5 @@ type MessageReference struct {
 	MessageId       uint64 `json:"message_id,string"`
 	ChannelId       uint64 `json:"channel_id,string"`
 	GuildId         uint64 `json:"guild_id,string"`
-	FailIfNotExists bool   `json:"fail_if_not_exists,omitempty"`
+	FailIfNotExists bool   `json:"fail_if_not_exists"`
 }
