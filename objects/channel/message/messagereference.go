7fe827a1519767850193904861e1d082023076fa
rxdn <29165304+rxdn@users.noreply.github.com>
2021-03-01 22:13:51 +0000

    Add fail_if_not_exists field

--- objects/channel/message/messagereference.go
+++ objects/channel/message/messagereference.go
@@ -1,7 +1,8 @@
 package message
 
 type MessageReference struct {
-	MessageId uint64 `json:"message_id,string"`
-	ChannelId uint64 `json:"channel_id,string"`
-	GuildId   uint64 `json:"guild_id,string"`
+	MessageId       uint64 `json:"message_id,string"`
+	ChannelId       uint64 `json:"channel_id,string"`
+	GuildId         uint64 `json:"guild_id,string"`
+	FailIfNotExists bool   `json:"fail_if_not_exists,omitempty"`
 }
