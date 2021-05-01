1ce9339d0a05504e71e9984fef95c5ce176af673
rxdn <29165304+rxdn@users.noreply.github.com>
2021-05-01 13:41:04 +0100

    Require ptr

--- objects/interaction/applicationcommandcallbackdata.go
+++ objects/interaction/applicationcommandcallbackdata.go
@@ -8,7 +8,7 @@ import (
 type ApplicationCommandCallbackData struct {
 	Tts             bool                   `json:"tts"`
 	Content         string                 `json:"content,omitempty"`
-	Embeds          []embed.Embed          `json:"embeds,omitempty"`
+	Embeds          []*embed.Embed          `json:"embeds,omitempty"`
 	AllowedMentions message.AllowedMention `json:"allowed_mentions,omitempty"`
 	Flags           uint                   `json:"flags"`
 }
