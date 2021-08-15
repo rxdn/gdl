63d9d23c48501a6d40e6025d3bd62f4b60653a74
rxdn <29165304+rxdn@users.noreply.github.com>
2021-08-15 19:13:12 +0100

    components

--- objects/interaction/applicationcommandcallbackdata.go
+++ objects/interaction/applicationcommandcallbackdata.go
@@ -3,12 +3,14 @@ package interaction
 import (
 	"github.com/rxdn/gdl/objects/channel/embed"
 	"github.com/rxdn/gdl/objects/channel/message"
+	"github.com/rxdn/gdl/objects/interaction/component"
 )
 
 type ApplicationCommandCallbackData struct {
 	Tts             bool                   `json:"tts"`
 	Content         string                 `json:"content,omitempty"`
-	Embeds          []*embed.Embed          `json:"embeds,omitempty"`
+	Embeds          []*embed.Embed         `json:"embeds,omitempty"`
 	AllowedMentions message.AllowedMention `json:"allowed_mentions,omitempty"`
 	Flags           uint                   `json:"flags"`
+	Components      []component.Component  `json:"components,omitempty"`
 }
