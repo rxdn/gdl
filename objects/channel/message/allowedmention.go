5b914c1591c630795a1c093c3e004c8e8e649fc5
rxdn <29165304+rxdn@users.noreply.github.com>
2021-09-02 13:36:44 +0100

    FIx allowed mentions bug

--- objects/channel/message/allowedmention.go
+++ objects/channel/message/allowedmention.go
@@ -3,10 +3,10 @@ package message
 import "github.com/rxdn/gdl/utils"
 
 type AllowedMention struct {
-	Parse       []AllowedMentionType      `json:"parse,omitempty"`
-	Roles       []utils.Uint64StringSlice `json:"roles,omitempty"`
-	Users       []utils.Uint64StringSlice `json:"users,omitempty"`
-	RepliedUser bool                      `json:"replied_user"`
+	Parse       []AllowedMentionType    `json:"parse,omitempty"`
+	Roles       utils.Uint64StringSlice `json:"roles,omitempty"`
+	Users       utils.Uint64StringSlice `json:"users,omitempty"`
+	RepliedUser bool                    `json:"replied_user"`
 }
 
 // Helper
