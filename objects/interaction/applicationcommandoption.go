9dd1c92edf606da9191eb88442d50f356aaacf6d
rxdn <29165304+rxdn@users.noreply.github.com>
2021-10-25 22:57:01 +0100

    Fix missing fields

--- objects/interaction/applicationcommandoption.go
+++ objects/interaction/applicationcommandoption.go
@@ -6,6 +6,7 @@ type ApplicationCommandInteractionDataOption struct {
 	Name    string                                    `json:"name"`
 	Value   interface{}                               `json:"value,omitempty"`
 	Options []ApplicationCommandInteractionDataOption `json:"options,omitempty"`
+	Focused bool                                      `json:"focused"`
 }
 
 type ApplicationCommandOption struct {
@@ -15,6 +16,7 @@ type ApplicationCommandOption struct {
 	Default      bool                             `json:"default"`
 	Required     bool                             `json:"required"`
 	Choices      []ApplicationCommandOptionChoice `json:"choices,omitempty"`
+	Autocomplete bool                             `json:"autocomplete"`
 	Options      []ApplicationCommandOption       `json:"options,omitempty"`
 	ChannelTypes []channel.ChannelType            `json:"channel_types,omitempty"`
 }
