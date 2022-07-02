232a9c8fc2394b6adc6b48cd1cf661ac3cc879e9
rxdn <29165304+rxdn@users.noreply.github.com>
2022-07-02 19:51:47 +0100

    Create Event generic type

--- gateway/payloads/events/eventtypes.go
+++ gateway/payloads/events/eventtypes.go
@@ -2,6 +2,53 @@ package events
 
 import "reflect"
 
+type Event interface {
+	Ready |
+	Resumed |
+	Reconnect |
+	InvalidSession |
+	ChannelCreate |
+	ChannelUpdate |
+	ChannelDelete |
+	ChannelPinsUpdate |
+	GuildCreate |
+	GuildUpdate |
+	GuildDelete |
+	GuildBanAdd |
+	GuildBanRemove |
+	GuildEmojisUpdate |
+	GuildIntegrationsUpdate |
+	GuildMemberAdd |
+	GuildMemberRemove |
+	GuildMemberUpdate |
+	GuildMembersChunk |
+	GuildRoleCreate |
+	GuildRoleUpdate |
+	GuildRoleDelete |
+	InviteCreate |
+	InviteDelete |
+	MessageCreate |
+	MessageUpdate |
+	MessageDelete |
+	MessageDeleteBulk |
+	MessageReactionAdd |
+	MessageReactionRemove |
+	MessageReactionRemoveAll |
+	MessageReactionRemoveEmoji |
+	PresenceUpdate |
+	ThreadCreate |
+	ThreadUpdate |
+	ThreadDelete |
+	ThreadListSync |
+	ThreadMembersUpdate |
+	ThreadMemberUpdate |
+	TypingStart |
+	UserUpdate |
+	VoiceServerUpdate |
+	VoiceStateUpdate |
+	WebhooksUpdate
+}
+
 var EventTypes = map[EventType]reflect.Type{
 	READY:                         reflect.TypeOf(Ready{}),
 	RESUMED:                       reflect.TypeOf(Resumed{}),
