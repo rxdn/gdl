506160e67ca5f49982e821d9904af3cb828ae8ce
Ben Hall <me@benhall.co>
2022-07-12 18:47:04 +0100

    Add permissions to Member

--- objects/member/member.go
+++ objects/member/member.go
@@ -14,6 +14,7 @@ type Member struct {
 	PremiumSince *time.Time              `json:"premium_since"` // when the user started boosting the guild
 	Deaf         bool                    `json:"deaf"`
 	Mute         bool                    `json:"mute"`
+	Permissions  string                  `json:"permissions"`
 }
 
 func (m *Member) HasRole(roleId uint64) bool {
